2410028309e7db93164a7ec6f513b95821ba6115
jcajasba <joel.cajas@telefonica.com>
2022-04-11 01:22:58 -0500

    Carga del proyecto

---
+++ Product.APIBusiness/Product.APIBusiness.API/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Users.APIBusiness.DBContext.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Users.APIBusiness.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/product")]
+    public class ProductController : Controller
+    {
+
+        protected readonly IProductRepository _ProductRepository;
+
+        public ProductController(IProductRepository ProductRepository)
+        {
+            _ProductRepository = ProductRepository;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="campus_id"></param>
+        /// <param name="active"></param>
+
+
+        /// <returns></returns>
+        [Produces("application/json")]
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("list-Products")]
+        public ActionResult List_Products(Int32 campus_id, int active)
+        {
+            var result = _ProductRepository.List_Products(campus_id, active);
+            return Json(result);
+        }
+
+    }
+}
---
+++ Product.APIBusiness/Product.APIBusiness.DBEntity/Model/ProductEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Users.APIBusiness.DBEntity.Model
+{
+    public class ProductEntity
+    {
+        public Int32 product_id { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+        public decimal width_product { get; set; }
+        public decimal long_product { get; set; }
+        public string unit_measurement { get; set; }
+        public decimal price { get; set; }
+        public string currency_type { get; set; }
+        public Int32 product_category_id { get; set; }
+        public string product_category_description { get; set; }
+        public int active { get; set; }
+        public Int32 campus_id { get; set; }
+        public decimal campus_description { get; set; }
+
+
+    }
+}
